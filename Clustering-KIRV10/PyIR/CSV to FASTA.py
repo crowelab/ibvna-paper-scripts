516c38a05a1beab4cba262123b4e886f9eef2ba7
crowelab <sambday@vvcenter.org>
2022-12-15 13:18:47 -0600

    Update CSV to FASTA.py

--- Clustering-KIRV10/PyIR/CSV to FASTA.py
+++ Clustering-KIRV10/PyIR/CSV to FASTA.py
@@ -1,18 +1,4 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.14.4
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
 
-# +
 import sys
 import pandas as pd
 import numpy as np
@@ -23,15 +9,8 @@ import csv
 import gzip
 import json
 import bson
-#import argparse
-
-#parser = argparse.ArgumentParser()
-
 
 files = ['KIRV10_LC.csv']
-# -
-
-#df= pd.read_excel('04.05.2017_Nigeria_DRC_EBOV Seq_1_.xlsx', sheetname='Sheet1')
 with open('KIRV_10LC.fasta', 'w') as fout:
     for f in files:
         with open(f,'r') as fin, open(f.split('.')[0] + '.fasta', 'w') as fout:
@@ -39,21 +18,3 @@ with open('KIRV_10LC.fasta', 'w') as fout:
             for row in reader:
                 fout.write('>' + row['ID'] + '\n')
                 fout.write(row['HC'] + '\n')
-
-
-# +
-#df.head()
-
-# +
-#d=df.to_dict()
-#print(d['Ab ID'])
-#print (d.keys())
-
-# +
-#with open('Nigeria1.fasta', 'w') as fout: 
-#    for key in d:
-#        fout.write('>' + d[key]['Ab ID'] + '\n')
-#        fout.write(d[key]['Heavy Sequence'] + '\n')
-# -
-
-
