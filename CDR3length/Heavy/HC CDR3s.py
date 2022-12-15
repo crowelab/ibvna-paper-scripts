001cfb1926a2ee139980257d27d95b2fb60b4d4f
crowelab <sambday@vvcenter.org>
2022-12-15 13:10:12 -0600

    Update HC CDR3s.py

--- CDR3length/Heavy/HC CDR3s.py
+++ CDR3length/Heavy/HC CDR3s.py
@@ -1,24 +1,8 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.14.4
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import norm
-# %matplotlib inline
 
-# +
 KIRV7_OL=pd.read_csv('KIRV007-overlaps-CDR3-counted-HC.csv')
 KIRV7_BM=pd.read_csv('KIRV007BM-CDR3-counted-HC.csv')
 KIRV7_PB=pd.read_csv('KIRV007PB-CDR3-counted-HC.csv')
@@ -39,14 +23,12 @@ plt.ylim(0,0.2)
 
 plt.savefig('KIRV007-HC-CDR3.png')
 
-# +
 KIRV8_OL=pd.read_csv('KIRV008-overlaps-CDR3-counted-HC.csv')
 KIRV8_BM=pd.read_csv('KIRV008BM-CDR3-counted-HC.csv')
 KIRV8_PB=pd.read_csv('KIRV008PB-CDR3-counted-HC.csv')
 
 KIRV8_OL.head()
 
-# +
 plt.figure(figsize=(30,30))
 sns.set(font_scale=1.5)
 sns.set_style("white")
@@ -58,21 +40,14 @@ sns.kdeplot(KIRV8_PB['heavycdr3'], color='#fee2e3', label='PB', linewidth=8, sha
 plt.xlim(0,40)
 plt.ylim(0,0.2)
 
-# plt.legend(prop={'size': 20})
-# plt.title('CDR3 Lenth Distribution', fontsize=20)
-# plt.xlabel('CDR3 Length', fontsize=15)
-# plt.ylabel('Frequency', fontsize=15)
-
 plt.savefig('KIRV008-HC-CDR3.png')
 
-# +
 KIRV9_OL=pd.read_csv('KIRV009-overlaps-CDR3-counted-HC.csv')
 KIRV9_BM=pd.read_csv('KIRV009BM-CDR3-counted-HC.csv')
 KIRV9_PB=pd.read_csv('KIRV009PB-CDR3-counted-HC.csv')
 
 KIRV9_OL.head()
 
-# +
 plt.figure(figsize=(30,30))
 sns.set(font_scale=1.5)
 sns.set_style("white")
@@ -84,21 +59,14 @@ sns.kdeplot(KIRV9_PB['heavycdr3'], color='#fee2e3', label='PB', linewidth=8, sha
 plt.xlim(0,40)
 plt.ylim(0,0.2)
 
-# plt.legend(prop={'size': 20})
-# plt.title('CDR3 Lenth Distribution', fontsize=20)
-# plt.xlabel('CDR3 Length', fontsize=15)
-# plt.ylabel('Frequency', fontsize=15)
-
 plt.savefig('KIRV009-HC-CDR3.png')
 
-# +
 KIRV10_OL=pd.read_csv('KIRV010-overlaps-CDR3-counted-HC.csv')
 KIRV10_BM=pd.read_csv('KIRV010BM-CDR3-counted-HC.csv')
 KIRV10_PB=pd.read_csv('KIRV010PB-CDR3-counted-HC.csv')
 
 KIRV10_OL.head()
 
-# +
 plt.figure(figsize=(30,30))
 sns.set(font_scale=1.5)
 sns.set_style("white")
@@ -110,12 +78,4 @@ sns.kdeplot(KIRV10_PB['heavycdr3'], color='#fee2e3', label='PB', linewidth=8, sh
 plt.xlim(0,40)
 plt.ylim(0,0.2)
 
-# plt.legend(prop={'size': 20})
-# plt.title('CDR3 Lenth Distribution', fontsize=20)
-# plt.xlabel('CDR3 Length', fontsize=15)
-# plt.ylabel('Frequency', fontsize=15)
-
 plt.savefig('KIRV010-HC-CDR3.png')
-# -
-
-
